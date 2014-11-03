438a670cc72fd7311fee222eccd250b22dcc7c17
Adam Sherwood <theherk@gmail.com>
2014-11-03 12:51:37 -0600

    Remove extras no longer needed.

--- links.sh
+++ links.sh
@@ -2,9 +2,7 @@
 
 # Force link setup for dotfiles
 
-ln -sf ~/dotfiles/.bash_profile ~/
 ln -sf ~/dotfiles/.bashrc ~/
-ln -sf ~/dotfiles/.dir_colors ~/
 ln -sf ~/dotfiles/.git-completion.bash ~/
 ln -sf ~/dotfiles/.git-completion.zsh ~/
 ln -sf ~/dotfiles/.gitconfig ~/
@@ -12,6 +10,5 @@ ln -sf ~/dotfiles/.gvimrc ~/
 ln -sf ~/dotfiles/.pypirc ~/
 ln -sf ~/dotfiles/.vim/ ~/
 ln -sf ~/dotfiles/.vimrc ~/
-ln -sf ~/dotfiles/.Xresources ~/
 ln -sf ~/dotfiles/.zshrc ~/
 
