36440431be3bce608349d420c67030951d82257d
Herkermer Sherwood <hsherwood@caltesting.org>
2015-03-22 20:05:02 -0500

    Remove links to files moved into .vim

--- links.sh
+++ links.sh
@@ -6,12 +6,10 @@ ln -sf ~/herk-commons/.bashrc ~/
 ln -sf ~/herk-commons/.git-completion.bash ~/
 ln -sf ~/herk-commons/.git-completion.zsh ~/
 ln -sf ~/herk-commons/.gitconfig ~/
-ln -sf ~/herk-commons/.gvimrc ~/
 ln -sf ~/herk-commons/.pypirc ~/
 ln -sf ~/herk-commons/.tmux.conf ~/
 ln -sf ~/herk-commons/.tmuxline ~/
 ln -sf ~/herk-commons/.vim/ ~/
-ln -sf ~/herk-commons/.vimrc ~/
 ln -sf ~/herk-commons/.zprofile ~/
 ln -sf ~/herk-commons/.zshrc ~/
 
