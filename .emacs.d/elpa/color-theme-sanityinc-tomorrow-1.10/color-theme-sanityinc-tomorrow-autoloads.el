e961864c78316ce981ae37b30965240377dec994
Herkermer Sherwood <theherk@gmail.com>
2015-06-21 22:19:16 -0500

    Initial entry of emacs (here we go)

---
+++ .emacs.d/elpa/color-theme-sanityinc-tomorrow-1.10/color-theme-sanityinc-tomorrow-autoloads.el
@@ -0,0 +1,52 @@
+;;; color-theme-sanityinc-tomorrow-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "color-theme-sanityinc-tomorrow" "color-theme-sanityinc-tomorrow.el"
+;;;;;;  (21893 61549 555325 53000))
+;;; Generated autoloads from color-theme-sanityinc-tomorrow.el
+
+(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'color-theme-sanityinc-tomorrow-night "color-theme-sanityinc-tomorrow" "\
+
+
+\(fn)" t nil)
+
+(autoload 'color-theme-sanityinc-tomorrow-day "color-theme-sanityinc-tomorrow" "\
+
+
+\(fn)" t nil)
+
+(autoload 'color-theme-sanityinc-tomorrow-bright "color-theme-sanityinc-tomorrow" "\
+
+
+\(fn)" t nil)
+
+(autoload 'color-theme-sanityinc-tomorrow-eighties "color-theme-sanityinc-tomorrow" "\
+
+
+\(fn)" t nil)
+
+(autoload 'color-theme-sanityinc-tomorrow-blue "color-theme-sanityinc-tomorrow" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("color-theme-sanityinc-tomorrow-pkg.el"
+;;;;;;  "sanityinc-tomorrow-blue-theme.el" "sanityinc-tomorrow-bright-theme.el"
+;;;;;;  "sanityinc-tomorrow-day-theme.el" "sanityinc-tomorrow-eighties-theme.el"
+;;;;;;  "sanityinc-tomorrow-night-theme.el") (21893 61549 594564
+;;;;;;  806000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; color-theme-sanityinc-tomorrow-autoloads.el ends here
---
+++ .emacs.d/elpa/goto-last-change-1.2/goto-last-change-pkg.el
@@ -0,0 +1,1 @@
+(define-package "goto-last-change" "1.2" "Move point through buffer-undo-list positions" 'nil)
