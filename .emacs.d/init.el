f1e874403a10870a876dc776397c3096faaee368
Herkermer Sherwood <hsherwood@caltesting.org>
2015-07-14 11:59:46 -0500

    Add melpa and helm configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+;; I'm so stupid reminders
+;; term-mode C-c intead of C-x
+;; term-mode C-c C-j to character mode C-c C-k back to default line mode
+
+;; to open second instance of named buffer rename-buffer
+
 (setq package-enable-at-startup nil) (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -58,3 +64,50 @@
   kept-new-versions 6
   kept-old-versions 2
   version-control t)
+
+;; MELPA
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; helm configuration
+;; ---
+
+(setq helm-ff-transformer-show-only-basename nil
+      helm-adaptive-history-file             "~/.emacs.d/data/helm-history"
+      helm-yank-symbol-first                 t
+      helm-move-to-line-cycle-in-source      t
+      helm-buffers-fuzzy-matching            t
+      helm-ff-auto-update-initial-value      t)
+
+(autoload 'helm-descbinds      "helm-descbinds" t)
+(autoload 'helm-eshell-history "helm-eshell"    t)
+(autoload 'helm-esh-pcomplete  "helm-eshell"    t)
+
+(global-set-key (kbd "C-h a")    #'helm-apropos)
+(global-set-key (kbd "C-h i")    #'helm-info-emacs)
+(global-set-key (kbd "C-h b")    #'helm-descbinds)
+
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "TAB")     #'helm-esh-pcomplete)
+              (define-key eshell-mode-map (kbd "C-c C-l") #'helm-eshell-history)))
+
+(global-set-key (kbd "C-x b")   #'helm-mini)
+(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+(global-set-key (kbd "C-x C-m") #'helm-M-x)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x C-r") #'helm-recentf)
+(global-set-key (kbd "C-x r l") #'helm-filtered-bookmarks)
+(global-set-key (kbd "M-y")     #'helm-show-kill-ring)
+(global-set-key (kbd "M-s o")   #'helm-swoop)
+(global-set-key (kbd "M-s /")   #'helm-multi-swoop)
+
+(require 'helm-config)
+(helm-mode t)
+(helm-adaptative-mode t)
+
+(global-set-key (kbd "C-x c!")   #'helm-calcul-expression)
+(global-set-key (kbd "C-x c:")   #'helm-eval-expression-with-eldoc)
+(define-key helm-map (kbd "M-o") #'helm-previous-source)
+
+(global-set-key (kbd "M-s s")   #'helm-ag)
