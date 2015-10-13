98d4b65362c813dfc09246fea8b796699fd05e40
Herkermer Sherwood <theherk@gmail.com>
2015-10-12 19:10:02 -0700

    Add resolution setting utils

---
+++ bin/resolution_1366x768.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# get mode definition
+# cvt 1366 768 60
+xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+xrandr --addmode VGA-0 1368x768_60.00
+xrandr --output VGA-0 --mode 1368x768_60.00
+xrandr --output VGA-0 --mode 1368x768_60.00
