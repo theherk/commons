d7393f5ffb76f6c7ba890b246c62e3d731431593
Herkermer Sherwood <theherk@gmail.com>
2015-11-16 08:33:57 -0800

    Update font settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,12 @@
 ;; local sudo /sudo::
 
 (setq package-enable-at-startup nil) (package-initialize)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
