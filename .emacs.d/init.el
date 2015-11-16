fa2e3a5471eafa39126fed760e34696d52bbece0
Herkermer Sherwood <theherk@gmail.com>
2015-11-16 08:34:34 -0800

    Remove old font settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,12 +27,6 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(tool-bar-mode nil)
  '(x-select-enable-clipboard t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
 (if window-system
     (tool-bar-mode -1)
   )
