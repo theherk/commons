1c3d8b20213081b42a8e4babc5ad6c7040c33652
Herkermer Sherwood <theherk@gmail.com>
2016-02-11 10:56:52 -0800

    Comment old emacs.d config and link new spacemacs.d

--- links.sh
+++ links.sh
@@ -12,7 +12,8 @@ ln -sf ~/herk-commons/.tmuxline ~/
 ln -sf ~/herk-commons/.vim/ ~/
 ln -sf ~/herk-commons/.zprofile ~/
 ln -sf ~/herk-commons/.zshrc ~/
-ln -sf ~/herk-commons/.emacs/ ~/
+#ln -sf ~/herk-commons/.emacs/ ~/
+ln -sf ~/herk-commons/.spacemacs.d/ ~/
 ln -sf ~/herk-commons/.i3/ ~/
 
 
