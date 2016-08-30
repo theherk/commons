b4cc566533f1fa8c1a64119cdf98af0ce67e676b
Herkermer Sherwood <theherk@gmail.com>
2016-08-29 18:03:14 -0700

    Modify resolution files

--- bin/resolution_1366x768.sh
+++ bin/resolution_1366x768.sh
@@ -2,7 +2,11 @@
 
 # get mode definition
 # cvt 1366 768 60
+# xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+# xrandr --addmode VGA-0 1368x768_60.00
+# xrandr --output VGA-0 --mode 1368x768_60.00
+# xrandr --output VGA-0 --mode 1368x768_60.00
 xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
-xrandr --addmode VGA-0 1368x768_60.00
-xrandr --output VGA-0 --mode 1368x768_60.00
-xrandr --output VGA-0 --mode 1368x768_60.00
+xrandr --addmode VGA-1 1368x768_60.00
+xrandr --output VGA-1 --mode 1368x768_60.00
+xrandr --output VGA-1 --mode 1368x768_60.00
