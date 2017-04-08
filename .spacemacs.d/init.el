17d5b5213d3a1ded438616558cd5730fd0e27410
Herkermer Sherwood <theherk@gmail.com>
2017-04-07 21:28:01 -0700

    Update formatting

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -4,9 +4,9 @@
 You should not put any user code in this function besides modifying the variable
 values."
   (setq-default
-   dotspacemacs-distribution 'spacemacs
-   dotspacemacs-configuration-layer-path '()
-   dotspacemacs-configuration-layers
+    dotspacemacs-distribution 'spacemacs
+    dotspacemacs-configuration-layer-path '()
+    dotspacemacs-configuration-layers
     '(
        auto-completion
        better-defaults
@@ -38,13 +38,13 @@ values."
        xkcd
        yaml
        )
-   dotspacemacs-additional-packages
-   '(
-     editorconfig
-     tramp-term
-     )
+    dotspacemacs-additional-packages
+    '(
+       editorconfig
+       tramp-term
+       )
     dotspacemacs-excluded-packages '()
-   dotspacemacs-delete-orphan-packages t))
+    dotspacemacs-delete-orphan-packages t))
 
 (defun dotspacemacs/init ()
   "Initialization function.
@@ -53,59 +53,59 @@ before layers configuration.
 You should not put any user code in there besides modifying the variable
 values."
   (setq-default
-   dotspacemacs-elpa-https t
-   dotspacemacs-elpa-timeout 5
-   dotspacemacs-check-for-update t
-   dotspacemacs-editing-style 'vim
-   dotspacemacs-verbose-loading nil
-   dotspacemacs-startup-banner 'random
-   dotspacemacs-startup-lists nil
-   dotspacemacs-startup-recent-list-size 5
-   dotspacemacs-scratch-mode 'markdown-mode
-   dotspacemacs-themes '(sanityinc-tomorrow-eighties
-                         spacemacs-dark
-                         spacemacs-light)
-   dotspacemacs-colorize-cursor-according-to-state t
-   dotspacemacs-default-font '("Ubuntu Mono"
-                               :size 16
-                               :weight normal
-                               :width normal
-                               :powerline-scale 1.1)
-   dotspacemacs-leader-key "SPC"
-   dotspacemacs-emacs-leader-key "M-m"
-   dotspacemacs-major-mode-leader-key ","
-   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
-   dotspacemacs-distinguish-gui-tab nil
-   dotspacemacs-command-key ":"
-   dotspacemacs-remap-Y-to-y$ t
-   dotspacemacs-default-layout-name "Default"
-   dotspacemacs-display-default-layout nil
-   dotspacemacs-auto-resume-layouts nil
-   dotspacemacs-auto-save-file-location 'cache
-   dotspacemacs-max-rollback-slots 5
-   dotspacemacs-use-ido nil
-   dotspacemacs-helm-resize nil
-   dotspacemacs-helm-no-header nil
-   dotspacemacs-helm-position 'bottom
-   dotspacemacs-enable-paste-micro-state t
-   dotspacemacs-which-key-delay 0.4
-   dotspacemacs-which-key-position 'bottom
-   dotspacemacs-loading-progress-bar t
-   dotspacemacs-fullscreen-at-startup nil
-   dotspacemacs-fullscreen-use-non-native nil
-   dotspacemacs-maximized-at-startup t
-   dotspacemacs-active-transparency 90
-   dotspacemacs-inactive-transparency 90
-   dotspacemacs-mode-line-unicode-symbols t
-   dotspacemacs-smooth-scrolling nil
-   dotspacemacs-line-numbers 'relative
-   dotspacemacs-smartparens-strict-mode t
-   dotspacemacs-highlight-delimiters 'current
-   dotspacemacs-persistent-server nil
-   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
-   dotspacemacs-default-package-repository nil
-   dotspacemacs-whitespace-cleanup 'trailing
-   ))
+    dotspacemacs-elpa-https t
+    dotspacemacs-elpa-timeout 5
+    dotspacemacs-check-for-update t
+    dotspacemacs-editing-style 'vim
+    dotspacemacs-verbose-loading nil
+    dotspacemacs-startup-banner 'random
+    dotspacemacs-startup-lists nil
+    dotspacemacs-startup-recent-list-size 5
+    dotspacemacs-scratch-mode 'markdown-mode
+    dotspacemacs-themes '(sanityinc-tomorrow-eighties
+                           spacemacs-dark
+                           spacemacs-light)
+    dotspacemacs-colorize-cursor-according-to-state t
+    dotspacemacs-default-font '("Ubuntu Mono"
+                                 :size 20
+                                 :weight normal
+                                 :width normal
+                                 :powerline-scale 1.1)
+    dotspacemacs-leader-key "SPC"
+    dotspacemacs-emacs-leader-key "M-m"
+    dotspacemacs-major-mode-leader-key ","
+    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+    dotspacemacs-distinguish-gui-tab nil
+    dotspacemacs-command-key ":"
+    dotspacemacs-remap-Y-to-y$ t
+    dotspacemacs-default-layout-name "Default"
+    dotspacemacs-display-default-layout nil
+    dotspacemacs-auto-resume-layouts nil
+    dotspacemacs-auto-save-file-location 'cache
+    dotspacemacs-max-rollback-slots 5
+    dotspacemacs-use-ido nil
+    dotspacemacs-helm-resize nil
+    dotspacemacs-helm-no-header nil
+    dotspacemacs-helm-position 'bottom
+    dotspacemacs-enable-paste-micro-state t
+    dotspacemacs-which-key-delay 0.4
+    dotspacemacs-which-key-position 'bottom
+    dotspacemacs-loading-progress-bar t
+    dotspacemacs-fullscreen-at-startup nil
+    dotspacemacs-fullscreen-use-non-native nil
+    dotspacemacs-maximized-at-startup t
+    dotspacemacs-active-transparency 90
+    dotspacemacs-inactive-transparency 90
+    dotspacemacs-mode-line-unicode-symbols t
+    dotspacemacs-smooth-scrolling nil
+    dotspacemacs-line-numbers 'relative
+    dotspacemacs-smartparens-strict-mode t
+    dotspacemacs-highlight-delimiters 'current
+    dotspacemacs-persistent-server nil
+    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+    dotspacemacs-default-package-repository nil
+    dotspacemacs-whitespace-cleanup 'trailing
+    ))
 
 (defun dotspacemacs/user-init ()
   "Initialization function for user code.
@@ -122,14 +122,14 @@ layers configuration. You are free to put any user code."
                                 (flycheck-mode 1)
                                 (semantic-mode 1)
                                 (setq flycheck-checker 'python-pylint
-                                      flycheck-checker-error-threshold 900
-                                      flycheck-pylintrc "~/.pylintrc")))
+                                  flycheck-checker-error-threshold 900
+                                  flycheck-pylintrc "~/.pylintrc")))
   ;; (setq python-shell-interpreter "ipython")
   ;; (setq python-shell-interpreter-args "--simple-prompt")
   (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
   (setq browse-url-browser-function 'browse-url-generic
-        engine/browser-function 'browse-url-generic
-        browse-url-generic-program "chromium")
+    engine/browser-function 'browse-url-generic
+    browse-url-generic-program "chromium")
   (editorconfig-mode 1)
   (add-hook 'after-change-major-mode-hook 'editorconfig-apply 'append)
   (add-hook 'js2-mode-hook
@@ -152,24 +152,24 @@ layers configuration. You are free to put any user code."
   (spaceline-toggle-buffer-size-off)
   (spaceline-toggle-minor-modes-off)
   (spaceline-toggle-purpose-off)
-)
+  )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-command "~/.cabal/bin/pandoc")
- '(markdown-open-command "~/.cabal/bin/pandoc")
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(markdown-command "~/.cabal/bin/pandoc")
+  '(markdown-open-command "~/.cabal/bin/pandoc")
   '(package-selected-packages
      (quote
        (flycheck-gometalinter winum unfill solarized-theme madhat2r-theme fuzzy zonokai-theme zenburn-theme zen-and-art-theme yapfify xterm-color xkcd ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme tramp-term toxi-theme toml-mode toc-org tide typescript-mode terraform-mode hcl-mode tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme spacemacs-theme spaceline powerline spacegray-theme soothe-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode sass-mode salt-mode mmm-jinja2 yaml-mode reverse-theme restart-emacs rainbow-delimiters railscasts-theme racer pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode professional-theme powershell popwin plantuml-mode planet-theme pip-requirements phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode pcre2el pastels-on-dark-theme paradox spinner orgit organic-green-theme org-projectile org-present org org-pomodoro alert log4e gntp org-plus-contrib org-download org-bullets open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme noctilux-theme niflheim-theme nginx-mode neotree naquadah-theme mwim mustang-theme multi-term move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minimal-theme material-theme markdown-toc markdown-mode majapahit-theme magit-gitflow macrostep lush-theme lorem-ipsum livid-mode skewer-mode simple-httpd live-py-mode linum-relative link-hint light-soap-theme less-css-mode js2-refactor multiple-cursors js2-mode js-doc jbeans-theme jazz-theme ir-black-theme inkpot-theme info+ indent-guide ido-vertical-mode hydra hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make projectile helm-gtags helm-gitignore request helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme haml-mode gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate golden-ratio go-guru go-eldoc gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md ggtags gandalf-theme flyspell-correct-helm flyspell-correct flycheck-rust seq flycheck-pos-tip pos-tip flycheck pkg-info epl flx-ido flx flatui-theme flatland-theme firebelly-theme fill-column-indicator farmhouse-theme fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight espresso-theme eshell-z eshell-prompt-extras esh-help engine-mode emoji-cheat-sheet-plus emmet-mode elisp-slime-nav editorconfig dumb-jump dracula-theme dockerfile-mode docker json-mode tablist magit-popup docker-tramp json-snatcher json-reformat django-theme diminish diff-hl define-word darktooth-theme autothemer darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode cyberpunk-theme csv-mode company-web web-completion-data company-tern dash-functional tern company-statistics company-go go-mode company-emoji company-anaconda company column-enforce-mode color-theme-sanityinc-solarized coffee-mode clues-theme clean-aindent-mode cherry-blossom-theme cargo rust-mode busybee-theme bubbleberry-theme birds-of-paradise-plus-theme bind-map bind-key badwolf-theme auto-yasnippet yasnippet auto-highlight-symbol auto-dictionary auto-compile packed apropospriate-theme anti-zenburn-theme anaconda-mode pythonic f dash s ample-zen-theme ample-theme alect-themes aggressive-indent afternoon-theme adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup quelpa package-build color-theme-sanityinc-tomorrow)))
- '(paradox-github-token t))
+  '(paradox-github-token t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
