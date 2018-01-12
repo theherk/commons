d33c991f0bd10b1dc81830e76f8ee330964eba3f
Herkermer Sherwood <theherk@gmail.com>
2018-01-12 09:37:50 -0800

    Update links

--- links.sh
+++ links.sh
@@ -2,16 +2,23 @@
 
 # Force link setup for dotfiles
 
+ln -sf ~/herk-commons/.i3/ ~/
+ln -sf ~/herk-commons/.config/dunst/ ~/.config/
+ln -sf ~/herk-commons/.config/i3status/ ~/.config/
+ln -sf ~/herk-commons/.spacemacs.d/ ~/
+ln -sf ~/herk-commons/.vim/ ~/
+ln -sf ~/herk-commons/bin/ ~/
 ln -sf ~/herk-commons/.bashrc ~/
+ln -sf ~/herk-commons/.editorconfig ~/
+ln -sf ~/herk-commons/.eslintrc ~/
 ln -sf ~/herk-commons/.git-completion.bash ~/
 ln -sf ~/herk-commons/.git-completion.zsh ~/
 ln -sf ~/herk-commons/.gitconfig ~/
-ln -sf ~/herk-commons/.editorconfig ~/
+ln -sf ~/herk-commons/.gitignore ~/
+ln -sf ~/herk-commons/.pypirc ~/
 ln -sf ~/herk-commons/.tmux.conf ~/
 ln -sf ~/herk-commons/.tmuxline ~/
-ln -sf ~/herk-commons/.vim/ ~/
+ln -sf ~/herk-commons/.xinitrc ~/
+ln -sf ~/herk-commons/.Xresources ~/
 ln -sf ~/herk-commons/.zprofile ~/
 ln -sf ~/herk-commons/.zshrc ~/
-#ln -sf ~/herk-commons/.emacs/ ~/
-ln -sf ~/herk-commons/.spacemacs.d/ ~/
-ln -sf ~/herk-commons/.i3/ ~/
