1185884ea8de26be307e36ddb54f79ebaa70f973
Adam Sherwood <theherk@gmail.com>
2020-12-17 17:56:06 +0100

    Remove snippets in incorrect location.

--- .emacs.d/snippets/python-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `python-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'python-mode
-                     '(("ipy" "import IPython; IPython.embed() # IPython breakpoint - Don't forget to delete me.$0" "IPython Breakpoint" nil nil nil "/home/h4s/.emacs.d/snippets/python-mode/ipython" nil nil)))
-
-
-;;; Do not edit! File generated at Fri Sep 18 14:53:13 2015
