e422429f73b45701e95bf98c340ff429946d3713
Adam Sherwood <theherk@gmail.com>
2020-12-28 16:13:19 +0100

    Add doom configuration.
    This will grow into a real boy.

---
+++ .config/doom/config.el
@@ -0,0 +1,13 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Adam Sherwood"
+      user-mail-address "theherk@gmail.com")
+
+(setq doom-font (font-spec :family "Ubuntu Mono" :size 20 :weight 'light)
+      doom-big-font (font-spec :family "Ubuntu" :size 36 :weight 'normal))
+
+(setq doom-theme 'doom-dracula)
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type nil)
