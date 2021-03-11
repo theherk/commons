cbf032b5912f330deb39e7ea78de2534f1171503
Adam Sherwood <theherk@gmail.com>
2021-03-11 17:28:31 +0100

    Yank links in org mode.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -14,6 +14,19 @@
 
 (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
 
+(defun theherk/org-link-yank (&optional arg)
+  "Extract URL from org-mode link and add it to kill ring."
+  (interactive "P")
+  (let* ((link (org-element-lineage (org-element-context) '(link) t))
+          (type (org-element-property :type link))
+          (url (org-element-property :path link))
+          (url (concat type ":" url)))
+    (kill-new url)
+    (message (concat "Copied URL: " url))))
+(map! :leader
+  :desc "yank org link"
+  "m l y" #'theherk/org-link-yank)
+
 (map! :leader
   :desc "toggle wrap"
   "t t" #'toggle-truncate-lines)
