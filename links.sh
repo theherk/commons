1a19eb26793849663c228911bba4b67f2588e068
Adam Sherwood <theherk@gmail.com>
2021-06-25 09:57:10 +0200

    Add starship configuration.

--- links.sh
+++ links.sh
@@ -5,6 +5,7 @@ ln -sf ~/commons/.bashrc ~/
 ln -sf ~/commons/.config/doom/ ~/.config/
 ln -sf ~/commons/.config/git/ ~/.config/
 ln -sf ~/commons/.config/termite/ ~/.config/
+ln -sf ~/commons/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/commons/.editorconfig ~/
 ln -sf ~/commons/.eslintrc ~/
 ln -sf ~/commons/.pypirc ~/
