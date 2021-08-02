8bb73ce755b000f83195553bf4335deb8ac9a790
Adam Sherwood <theherk@gmail.com>
2021-08-02 15:31:44 +0200

    Remove agenda from custom.

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-directory "~/projects/dnb.no/adam.sherwood/notes")
- '(org-agenda-files (list org-directory)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
