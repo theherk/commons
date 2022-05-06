2c0fa49b13804e159594ed69e3365daa1dd80a97
Adam Sherwood <theherk@gmail.com>
2022-05-06 19:57:28 +0200

    Add nu config to links.

--- links.sh
+++ links.sh
@@ -4,6 +4,7 @@ ln -sf ~/commons/.config/alacritty/ ~/.config/
 ln -sf ~/commons/.config/doom/ ~/.config/
 ln -sf ~/commons/.config/git/ ~/.config/
 ln -sf ~/commons/.config/helix/ ~/.config/
+ln -sf ~/commons/.config/nu/ ~/.config/
 ln -sf ~/commons/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/commons/.editorconfig ~/
 ln -sf ~/commons/.eslintrc ~/
