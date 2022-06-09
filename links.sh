20da31021f41170b725d8efbf049d2ec1950e3a2
Adam Sherwood <theherk@gmail.com>
2022-06-09 18:44:10 +0200

    Add lazygit configuration.

--- links.sh
+++ links.sh
@@ -5,6 +5,7 @@ ln -sf ~/commons/.config/doom/ ~/.config/
 ln -sf ~/commons/.config/git/ ~/.config/
 ln -sf ~/commons/.config/gitui/ ~/.config/
 ln -sf ~/commons/.config/helix/ ~/.config/
+ln -sf ~/commons/.config/lazygit/ ~/.config/
 ln -sf ~/commons/.config/nu/ ~/.config/
 ln -sf ~/commons/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/commons/.config/wezterm/ ~/.config/
