76d67bd26715a17506d454f85d833255e475f912
Adam Sherwood <theherk@gmail.com>
2022-06-15 01:46:26 +0200

    Add kaolin dark theme.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,31 +1,50 @@
 local wezterm = require 'wezterm';
-local TRIANGLE = utf8.char(0x25B2)
+
+-- Kaolin Dark
+local C_ACTIVE_BG = "#4b5254";
+local C_ACTIVE_FG = "#e4e4e8";
+local C_BG = "#18181b";
+local C_HL_1 = "#F2c866";
+local C_HL_2 = "#E36d5b";
+local C_INACTIVE_FG = "#4b5254";
+
+-- Night Owl
+-- local C_ACTIVE_BG = "#674ba5";
+-- local C_ACTIVE_FG = "#e2dded";
+-- local C_BG = "#011627";
+-- local C_HL_1 = "#7fdbca";
+-- local C_HL_2 = "#addb67";
+-- local C_INACTIVE_FG = "#8ba2b6";
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
   if tab.is_active then
     return {
-      {Foreground={Color="#7fdbca"}},
-      {Text=" " .. tab.tab_index+1 .. ": "},
-      {Foreground={Color="#e2dded"}},
+      {Foreground={Color=C_HL_1}},
+      {Text=" " .. tab.tab_index+1},
+      {Foreground={Color=C_HL_2}},
+      {Text=": "},
+      {Foreground={Color=C_ACTIVE_FG}},
       {Text=tab.active_pane.title .. " "},
-      {Background={Color="#011627"}},
-      {Foreground={Color="#7fdbca"}},
+      {Background={Color=C_BG}},
+      {Foreground={Color=C_HL_1}},
       {Text="|"},
     }
   end
   return {
-    {Foreground={Color="#7fdbca"}},
-    {Text=" " .. tab.tab_index+1 .. ": "},
-    {Foreground={Color="#8ba2b6"}},
+    {Foreground={Color=C_HL_1}},
+    {Text=" " .. tab.tab_index+1},
+    {Foreground={Color=C_HL_2}},
+    {Text=": "},
+    {Foreground={Color=C_INACTIVE_FG}},
     {Text=tab.active_pane.title .. " "},
-    {Foreground={Color="#7fdbca"}},
+    {Foreground={Color=C_HL_1}},
     {Text="|"},
   }
 end)
 
 return {
   leader = { key="a", mods="CTRL"},
-  color_scheme = "Night Owl",
+  color_scheme = "kaolin-dark",
   font = wezterm.font("VictorMono Nerd Font"),
   font_size = 20.0,
   hide_tab_bar_if_only_one_tab = true,
@@ -35,23 +54,22 @@ return {
 
   colors = {
     tab_bar = {
-      background = "#011627",
+      background = C_BG,
       new_tab = {
-        bg_color = "#011627",
-        fg_color = "#addb67",
+        bg_color = C_BG,
+        fg_color = C_HL_1,
       },
       active_tab = {
-        bg_color = "#674ba5",
-        fg_color = "#e2dded",
-        intensity = "Bold",
+        bg_color = C_ACTIVE_BG,
+        fg_color = C_ACTIVE_FG,
       },
       inactive_tab = {
-        bg_color = "#011627",
-        fg_color = "#8ba2b6",
+        bg_color = C_BG,
+        fg_color = C_INACTIVE_FG,
       },
       inactive_tab_hover = {
-        bg_color = "#7e57c2",
-        fg_color = "#8ba2b6",
+        bg_color = C_BG,
+        fg_color = C_INACTIVE_FG,
       },
     },
   },
