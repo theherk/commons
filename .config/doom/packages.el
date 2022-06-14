de60c7e07b7a18dfb8da0d903f05e4a4ffe79bf7
Adam Sherwood <theherk@gmail.com>
2022-06-14 21:22:24 +0200

    Add hackernews reader.

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -2,7 +2,9 @@
 ;;; $DOOMDIR/packages.el
 
 (package! exec-path-from-shell)
+(package! hackernews)
 (package! night-owl-theme)
+;; (package! md4rd)
 (package! org-modern)
 (package! ox-jira)
 (package! rego-mode)
