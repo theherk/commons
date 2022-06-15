00cd9c78db49f3f14a140f7ed456b529fd1ca7ee
Adam Sherwood <theherk@gmail.com>
2022-06-15 21:00:52 +0200

    Use kaolin-bubblegum.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,20 @@
 local wezterm = require 'wezterm';
 
+-- Kaolin Bubblegum
+local C_ACTIVE_BG = "#454459";
+local C_ACTIVE_FG = "#6bd9db";
+local C_BG = "#14171e";
+local C_HL_1 = "#41b0f3";
+local C_HL_2 = "#c79af4";
+local C_INACTIVE_FG = "#575673";
+
 -- Kaolin Dark
-local C_ACTIVE_BG = "#4b5254";
-local C_ACTIVE_FG = "#e4e4e8";
-local C_BG = "#18181b";
-local C_HL_1 = "#F2c866";
-local C_HL_2 = "#E36d5b";
-local C_INACTIVE_FG = "#4b5254";
+-- local C_ACTIVE_BG = "#4b5254";
+-- local C_ACTIVE_FG = "#e4e4e8";
+-- local C_BG = "#18181b";
+-- local C_HL_1 = "#f2c866";
+-- local C_HL_2 = "#e36d5b";
+-- local C_INACTIVE_FG = "#4b5254";
 
 -- Night Owl
 -- local C_ACTIVE_BG = "#674ba5";
@@ -44,7 +52,7 @@ end)
 
 return {
   leader = { key="a", mods="CTRL"},
-  color_scheme = "kaolin-dark",
+  color_scheme = "kaolin-bubblegum",
   font = wezterm.font("VictorMono Nerd Font"),
   font_size = 20.0,
   hide_tab_bar_if_only_one_tab = true,
@@ -57,7 +65,7 @@ return {
       background = C_BG,
       new_tab = {
         bg_color = C_BG,
-        fg_color = C_HL_1,
+        fg_color = C_HL_2,
       },
       active_tab = {
         bg_color = C_ACTIVE_BG,
