77070b87aa962431d77b1077b976cd72f425153c
Adam Sherwood <theherk@gmail.com>
2022-06-18 20:41:45 +0200

    Add link note for org sync.

--- macup.sh
+++ macup.sh
@@ -109,3 +109,5 @@ defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 echo "pinentry-program /usr/local/bin/pinentry-mac" >>~/.gnupg/gpg-agent.conf
 # Install apptivate.
 git clone https://github.com/jeffreytse/zsh-vi-mode \\n $ZSH_CUSTOM/plugins/zsh-vi-mode
+# Setup dropbox org sync.
+ln -sf ~/Dropbox/org ~/
