bbd9ddd3ef226c8613118136ed655245a36ec434
Adam Sherwood <theherk@gmail.com>
2022-08-01 20:38:09 +0200

    Add and use tokyonight for a minute.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require 'wezterm';
 
 -- Kaolin Bubblegum
-local C_ACTIVE_BG = "#454459";
-local C_ACTIVE_FG = "#6bd9db";
-local C_BG = "#14171e";
-local C_HL_1 = "#41b0f3";
-local C_HL_2 = "#c79af4";
-local C_INACTIVE_FG = "#575673";
+-- local C_ACTIVE_BG = "#454459";
+-- local C_ACTIVE_FG = "#6bd9db";
+-- local C_BG = "#14171e";
+-- local C_HL_1 = "#41b0f3";
+-- local C_HL_2 = "#c79af4";
+-- local C_INACTIVE_FG = "#575673";
 
 -- Kaolin Dark
 -- local C_ACTIVE_BG = "#4b5254";
@@ -23,6 +23,14 @@ local C_INACTIVE_FG = "#575673";
 -- local C_HL_1 = "#7fdbca";
 -- local C_HL_2 = "#addb67";
 -- local C_INACTIVE_FG = "#8ba2b6";
+
+-- tokyonight
+local C_ACTIVE_BG = "#454459";
+local C_ACTIVE_FG = "#7dcfff";
+local C_BG = "#1a1b26";
+local C_HL_1 = "#7aa2f7";
+local C_HL_2 = "#bb9af7";
+local C_INACTIVE_FG = "#575673";
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
   if tab.is_active then
@@ -52,7 +60,8 @@ end)
 
 return {
   leader = { key="a", mods="CTRL"},
-  color_scheme = "kaolin-bubblegum",
+  -- color_scheme = "kaolin-bubblegum",
+  color_scheme = "tokyonight",
   font = wezterm.font("VictorMono Nerd Font"),
   font_size = 20.0,
   tab_bar_at_bottom = true,
