4e71270b0a7c6780c438c695895b9906964bd91b
Adam Sherwood <theherk@gmail.com>
2022-08-01 20:38:34 +0200

    Link lazygit to other config location.

--- links.sh
+++ links.sh
@@ -6,6 +6,7 @@ ln -sf ~/commons/.config/git/ ~/.config/
 ln -sf ~/commons/.config/gitui/ ~/.config/
 ln -sf ~/commons/.config/helix/ ~/.config/
 ln -sf ~/commons/.config/lazygit/ ~/.config/
+ln -sf ~/commons/.config/lazygit/lazygit.yml ~/Library/Application\ Support/lazygit/config.yml
 ln -sf ~/commons/.config/nu/ ~/.config/
 ln -sf ~/commons/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/commons/.config/wezterm/ ~/.config/
