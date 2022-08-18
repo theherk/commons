7bc50ec24b1b87dd7b9597b1a5a05f3b35369790
Adam Sherwood <theherk@gmail.com>
2022-08-18 20:38:39 +0200

    Switch to Prot's ef-themes.

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+(package! ef-themes)
 (package! exec-path-from-shell)
 (package! hackernews)
-(package! kaolin-themes)
 ;; (package! night-owl-theme)
 ;; (package! md4rd)
 (package! org-modern)
