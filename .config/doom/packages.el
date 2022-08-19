ba7668db9a4c1922f9aacab936e25aaa61ed3694
Adam Sherwood <theherk@gmail.com>
2022-08-19 13:07:10 +0200

    Reorder packages.

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,15 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+;; org-roam needs unpinned to use latest.
+(unpin! org-roam)
+
 (package! ef-themes)
 (package! exec-path-from-shell)
 (package! hackernews)
 (package! org-modern)
+(package! org-roam-ui)
 (package! ox-gfm)
-;; (package! ox-jira)
+(package! ox-jira)
 (package! ox-pandoc)
 (package! rego-mode)
 
-;; org-roam needs unpinned to use latest.
-(unpin! org-roam)
-(package! org-roam-ui)
