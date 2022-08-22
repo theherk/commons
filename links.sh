a3d72940f9fe911383036920ada24ea5666cc7ce
Adam Sherwood <theherk@gmail.com>
2022-08-22 10:09:41 +0200

    Add aliases and functions to link setter.

--- links.sh
+++ links.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ln -sf ~/commons/.aliases ~/
 ln -sf ~/commons/.config/alacritty/ ~/.config/
 ln -sf ~/commons/.config/doom/ ~/.config/
 ln -sf ~/commons/.config/git/ ~/.config/
@@ -12,6 +13,7 @@ ln -sf ~/commons/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/commons/.config/wezterm/ ~/.config/
 ln -sf ~/commons/.editorconfig ~/
 ln -sf ~/commons/.eslintrc ~/
+ln -sf ~/commons/.functions ~/
 ln -sf ~/commons/.pypirc ~/
 ln -sf ~/commons/.tmux.conf ~/
 ln -sf ~/commons/.zprofile ~/
