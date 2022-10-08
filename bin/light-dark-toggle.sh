46f2a9b32c24cae6061e382240b60addbfdd418d
Adam Sherwood <theherk@gmail.com>
2022-10-08 13:45:14 +0200

    Add light dark toggle script.

---
+++ bin/light-dark-toggle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+cd ~/commons
+if grep -q "light = true" ~/.config/git/config; then
+    git apply -R patch/delta-light.patch
+    git apply -R patch/emacs-light.patch
+    git apply -R patch/gitui-light.patch
+    git apply -R patch/helix-light.patch
+    git apply -R patch/wezterm-light.patch
+else
+    git apply patch/delta-light.patch
+    git apply patch/emacs-light.patch
+    git apply patch/gitui-light.patch
+    git apply patch/helix-light.patch
+    git apply patch/wezterm-light.patch
+fi
