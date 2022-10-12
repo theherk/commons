b3e30ebd44a024326202630175995a943e2ad434
Adam Sherwood <theherk@gmail.com>
2022-10-12 15:25:52 +0200

    Add bat patch to toggle.

--- bin/light-dark-toggle.sh
+++ bin/light-dark-toggle.sh
@@ -2,12 +2,14 @@
 
 cd ~/commons
 if grep -q "light = true" ~/.config/git/config; then
+    git apply -R patch/bat-light.patch
     git apply -R patch/delta-light.patch
     git apply -R patch/emacs-light.patch
     git apply -R patch/gitui-light.patch
     git apply -R patch/helix-light.patch
     git apply -R patch/wezterm-light.patch
 else
+    git apply patch/bat-light.patch
     git apply patch/delta-light.patch
     git apply patch/emacs-light.patch
     git apply patch/gitui-light.patch
