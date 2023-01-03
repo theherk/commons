39b4ac196ae84a9e90d9d4385c71583f2ba89a9a
Adam Sherwood <theherk@gmail.com>
2023-01-03 13:05:53 +0100

    Remove extra comments.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,24 +17,6 @@ else
   C_INACTIVE_FG = bg:complement_ryb():lighten(0.3);
 end
 
--- Overrides if scheme doesn't exist.
-
--- Kaolin Bubblegum
--- local C_ACTIVE_BG = "#454459";
--- local C_ACTIVE_FG = "#6bd9db";
--- local C_BG = "#14171e";
--- local C_HL_1 = "#41b0f3";
--- local C_HL_2 = "#c79af4";
--- local C_INACTIVE_FG = "#575673";
-
--- Kaolin Dark
--- local C_ACTIVE_BG = "#4b5254";
--- local C_ACTIVE_FG = "#e4e4e8";
--- local C_BG = "#18181b";
--- local C_HL_1 = "#f2c866";
--- local C_HL_2 = "#e36d5b";
--- local C_INACTIVE_FG = "#4b5254";
-
 scheme.tab_bar = {
   background = C_BG,
   new_tab = {
