f861c0dc8b7f5cbb4fdfe905645350427e5986d5
Adam Sherwood <theherk@gmail.com>
2023-05-01 15:21:00 +0200

    Add todo comments.

--- .config/lvim/lua/user/plugin.lua
+++ .config/lvim/lua/user/plugin.lua
@@ -1,5 +1,12 @@
 lvim.plugins = {
   {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
