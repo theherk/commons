1f929b5f56071344c029ea6d24333fcef763550e
Adam Sherwood <theherk@gmail.com>
2023-05-03 09:55:40 +0200

    Add ensure installed for treesitter.

--- .config/lvim/lua/user/misc.lua
+++ .config/lvim/lua/user/misc.lua
@@ -4,4 +4,12 @@ lvim.builtin.telescope.extensions.projects = {
   },
 }
 
+lvim.builtin.treesitter.ensure_installed = {
+  "comment",
+  "html",
+  "markdown_inline",
+  "python",
+  "regex"
+}
+
 lvim.builtin.treesitter.auto_install = true
