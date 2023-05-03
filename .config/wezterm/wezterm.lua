7df71b68e55afc30bcd17b1023f338d1dcefba72
Adam Sherwood <theherk@gmail.com>
2023-05-03 17:06:35 +0200

    Add panel and workspace handling for wezterm.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,12 +10,15 @@ local C_BG = scheme.background;
 local C_HL_1 = scheme.ansi[5];
 local C_HL_2 = scheme.ansi[4];
 local C_INACTIVE_FG;
+local C_PANEL_BG;
 local bg = wezterm.color.parse(scheme.background);
 local _, _, l, _ = bg:hsla();
 if l > 0.5 then
   C_INACTIVE_FG = bg:complement_ryb():darken(0.3);
+  C_PANEL_BG = bg:darken(0.1);
 else
   C_INACTIVE_FG = bg:complement_ryb():lighten(0.3);
+  C_PANEL_BG = bg:lighten(0.1);
 end
 
 local function tconcat(t1, t2)
@@ -113,6 +116,9 @@ wezterm.on(
 return {
   leader = { key = "a", mods = "CTRL" },
   check_for_updates = false,
+  command_palette_font_size = 24,
+  command_palette_bg_color = C_PANEL_BG,
+  command_palette_fg_color = C_ACTIVE_FG,
   color_schemes = {
     [selected_scheme] = scheme
   },
@@ -138,46 +144,72 @@ return {
     },
   },
   keys = {
-    { key = "a", mods = "LEADER|CTRL", action = act.SendString "\x01" },
+    { key = "a",  mods = "LEADER|CTRL", action = act.SendString "\x01" },
+
+    -- Workpace and Pallette
+    { key = "m",  mods = "LEADER",      action = act.ShowLauncher },
+    { key = "P",  mods = "SUPER|SHIFT", action = act.ActivateCommandPalette },
+    { key = "\t", mods = "LEADER",      action = act.SwitchWorkspaceRelative(1) },
+
+    {
+      key = "W",
+      mods = "LEADER",
+      action = act.PromptInputLine {
+        description = wezterm.format {
+          { Foreground = { Color = C_ACTIVE_FG } },
+          { Text = "New workspace name:" },
+        },
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:perform_action(
+              act.SwitchToWorkspace {
+                name = line,
+              },
+              pane
+            )
+          end
+        end),
+      },
+    },
 
     -- Mode
-    { key = "x", mods = "LEADER",      action = act.ActivateCopyMode },
-    { key = " ", mods = "LEADER",      action = act.QuickSelect },
+    { key = "x", mods = "LEADER", action = act.ActivateCopyMode },
+    { key = " ", mods = "LEADER", action = act.QuickSelect },
 
     -- Paste; Cmd-v or C-a p
-    { key = "p", mods = "LEADER",      action = act.PasteFrom "Clipboard" },
+    { key = "p", mods = "LEADER", action = act.PasteFrom "Clipboard" },
 
     -- Pane Management
-    { key = "s", mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-    { key = "v", mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-    { key = "w", mods = "LEADER",      action = act.CloseCurrentPane { confirm = false } },
-    { key = "H", mods = "LEADER",      action = act.AdjustPaneSize { "Left", 5 } },
-    { key = "J", mods = "LEADER",      action = act.AdjustPaneSize { "Down", 5 } },
-    { key = "K", mods = "LEADER",      action = act.AdjustPaneSize { "Up", 5 } },
-    { key = "L", mods = "LEADER",      action = act.AdjustPaneSize { "Right", 5 } },
-    { key = "r", mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
-    { key = "z", mods = "LEADER",      action = act.TogglePaneZoomState },
-    { key = "Z", mods = "LEADER",      action = act.TogglePaneZoomState },
+    { key = "s", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+    { key = "v", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = "w", mods = "LEADER", action = act.CloseCurrentPane { confirm = false } },
+    { key = "H", mods = "LEADER", action = act.AdjustPaneSize { "Left", 5 } },
+    { key = "J", mods = "LEADER", action = act.AdjustPaneSize { "Down", 5 } },
+    { key = "K", mods = "LEADER", action = act.AdjustPaneSize { "Up", 5 } },
+    { key = "L", mods = "LEADER", action = act.AdjustPaneSize { "Right", 5 } },
+    { key = "r", mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+    { key = "Z", mods = "LEADER", action = act.TogglePaneZoomState },
 
     -- Navigation
-    { key = "h", mods = "LEADER",      action = act.ActivatePaneDirection "Left" },
-    { key = "l", mods = "LEADER",      action = act.ActivatePaneDirection "Right" },
-    { key = "k", mods = "LEADER",      action = act.ActivatePaneDirection "Up" },
-    { key = "j", mods = "LEADER",      action = act.ActivatePaneDirection "Down" },
-    { key = "]", mods = "LEADER",      action = act.ActivateTabRelative(1) },
-    { key = "[", mods = "LEADER",      action = act.ActivateTabRelative(-1) },
-    { key = "}", mods = "LEADER",      action = act.MoveTabRelative(1) },
-    { key = "{", mods = "LEADER",      action = act.MoveTabRelative(-1) },
-    { key = "1", mods = "LEADER",      action = act.ActivateTab(0) },
-    { key = "2", mods = "LEADER",      action = act.ActivateTab(1) },
-    { key = "3", mods = "LEADER",      action = act.ActivateTab(2) },
-    { key = "4", mods = "LEADER",      action = act.ActivateTab(3) },
-    { key = "5", mods = "LEADER",      action = act.ActivateTab(4) },
-    { key = "6", mods = "LEADER",      action = act.ActivateTab(5) },
-    { key = "7", mods = "LEADER",      action = act.ActivateTab(6) },
-    { key = "8", mods = "LEADER",      action = act.ActivateTab(7) },
-    { key = "9", mods = "LEADER",      action = act.ActivateTab(8) },
-    { key = "0", mods = "LEADER",      action = act.ActivateTab(-1) },
-    { key = "0", mods = "SUPER",       action = act.ActivateTab(-1) },
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection "Left" },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection "Right" },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection "Up" },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection "Down" },
+    { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+    { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+    { key = "}", mods = "LEADER", action = act.MoveTabRelative(1) },
+    { key = "{", mods = "LEADER", action = act.MoveTabRelative(-1) },
+    { key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+    { key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+    { key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+    { key = "4", mods = "LEADER", action = act.ActivateTab(3) },
+    { key = "5", mods = "LEADER", action = act.ActivateTab(4) },
+    { key = "6", mods = "LEADER", action = act.ActivateTab(5) },
+    { key = "7", mods = "LEADER", action = act.ActivateTab(6) },
+    { key = "8", mods = "LEADER", action = act.ActivateTab(7) },
+    { key = "9", mods = "LEADER", action = act.ActivateTab(8) },
+    { key = "0", mods = "LEADER", action = act.ActivateTab(-1) },
+    { key = "0", mods = "SUPER",  action = act.ActivateTab(-1) },
   },
 }
