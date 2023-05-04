505130ce7521689cd39df99b57c832b0f6558002
Adam Sherwood <theherk@gmail.com>
2023-05-04 17:30:24 +0200

    Add directory finding cheat.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,6 +65,7 @@ if status is-interactive
     abbr -a awswhoami --position command aws sts get-caller-identity
     abbr -a bubu --position command "brew update && brew outdated && brew upgrade && brew cleanup"
     abbr -a c --position command codium
+    abbr -a d --position command 'fd -H -t d | sk'
     abbr -a clock --position command rsclock -c
     abbr -a dumplynx --position anywhere --set-cursor "%| lynx -dump -stdin"
     abbr -a dumpw3m --position anywhere --set-cursor "%| w3m -dump -T text/html"
