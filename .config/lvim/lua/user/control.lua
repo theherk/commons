21d29539723e1dfe5b42eda6c9e4a1bea1d8c7ff
Adam Sherwood <theherk@gmail.com>
2023-06-06 14:33:15 +0200

    Add zen mode shortcut.

--- .config/lvim/lua/user/control.lua
+++ .config/lvim/lua/user/control.lua
@@ -2,14 +2,14 @@ local actions = require("telescope.actions")
 
 vim.g.maplocalleader = ","
 
+lvim.builtin.telescope.defaults.mappings.i = { ["<esc>"] = actions.close }
+lvim.builtin.telescope.defaults.path_display = { "truncate" }
+
 lvim.builtin.which_key.mappings["J"] = { "<cmd>TSJToggle<CR>", "Treesj Toggle" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["ss"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Current Buf Fuzzy" }
 lvim.builtin.which_key.mappings["/"] = { "<cmd>Telescope live_grep<CR>", "Grep" }
-lvim.builtin.telescope.defaults.mappings.i = {
-  ["<esc>"] = actions.close,
-}
-lvim.builtin.telescope.defaults.path_display = { "truncate" }
+lvim.builtin.which_key.mappings["Z"] = { "<cmd>ZenMode<CR>", "Zen" }
 
 lvim.builtin.which_key.mappings["n"] = {
   name = "Neorg",
