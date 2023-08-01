74a907cb156dd21852dc577669535bf3bf76e0e3
Adam Sherwood <theherk@gmail.com>
2023-08-01 11:08:17 +0200

    Show hidden back off by default in xplr.

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,11 +1,12 @@
 version = "0.21.0"
 local xplr = xplr
+-- xplr.config.general.show_hidden = true
+
 local home = os.getenv("HOME")
 package.path = home
     .. "/.config/xplr/plugins/?/init.lua;"
     .. home
     .. "/.config/xplr/plugins/?.lua;"
     .. package.path
-
 require "icons".setup()
 require "zentable".setup()
