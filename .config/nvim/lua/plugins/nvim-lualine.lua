dc9a90d8613249800417c52678f1306255ddccb9
Adam Sherwood <theherk@gmail.com>
2023-08-24 21:05:51 +0200

    Improve lazy lualine configuration.

--- .config/nvim/lua/plugins/nvim-lualine.lua
+++ .config/nvim/lua/plugins/nvim-lualine.lua
@@ -1,5 +1,6 @@
 local custom = require("lualine.themes.tokyonight")
 custom.normal.c.bg = "none"
+local icons = require("lazyvim.config").icons
 
 return {
   {
@@ -12,6 +13,28 @@ return {
           "mode",
           separator = { left = "" },
           right_padding = 2,
+        },
+      }
+      opts.sections.lualine_c = {
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+        {
+          function()
+            return require("nvim-navic").get_location()
+          end,
+          cond = function()
+            return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
+          end,
+          padding = { left = 1, right = 0 },
         },
       }
       opts.sections.lualine_z = {
