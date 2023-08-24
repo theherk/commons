70963942c92641b210076cf913f78dc1831dbe19
Adam Sherwood <theherk@gmail.com>
2023-08-24 20:50:54 +0200

    Use bubble separators for lualine.

--- .config/nvim/lua/plugins/nvim-lualine.lua
+++ .config/nvim/lua/plugins/nvim-lualine.lua
@@ -6,6 +6,23 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       opts.options.theme = custom
+      opts.options.section_separators = { left = "", right = "" }
+      opts.sections.lualine_a = {
+        {
+          "mode",
+          separator = { left = "" },
+          right_padding = 2,
+        },
+      }
+      opts.sections.lualine_z = {
+        {
+          function()
+            return " " .. os.date("%R")
+          end,
+          separator = { right = "" },
+          left_padding = 2,
+        },
+      }
     end,
   },
 }
