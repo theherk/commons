97d000711c84c8e7cf3144bc9293f4e4f9db0c65
Adam Sherwood <theherk@gmail.com>
2023-08-26 12:05:56 +0200

    Add extra treesitter parsers.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -5,4 +5,28 @@ return {
       colorscheme = "tokyonight",
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "comment",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "norg",
+        "org",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
