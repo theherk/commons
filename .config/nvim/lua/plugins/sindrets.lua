bcc93c515176c175d05169f2f2c29be678197197
Adam Sherwood <theherk@gmail.com>
2023-08-26 10:37:39 +0200

    Add comments about keys function.

--- .config/nvim/lua/plugins/sindrets.lua
+++ .config/nvim/lua/plugins/sindrets.lua
@@ -2,5 +2,16 @@ return {
   {
     "sindrets/diffview.nvim",
     event = "BufRead",
+    -- Feels like this should be better.
+    -- However, it breaks the whicheky register.
+    -- keys = {
+    --   { "Dd", "<cmd>DiffviewOpen<cr>", desc = "DiffviewOpen" },
+    --   { "Dc", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
+    --   { "Df", "<cmd>DiffviewToggleFiles<cr>", desc = "DiffviewToggleFiles" },
+    --   { "Dh", "<cmd>DiffviewFileHistory<cr>", desc = "DiffviewFileHistory" },
+    --   { "Dl", "<cmd>DiffviewLog<cr>", desc = "DiffviewLog" },
+    --   { "Dr", "<cmd>DiffviewRefresh<cr>", desc = "DiffviewRefresh" },
+    --   { "Dq", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
+    -- },
   },
 }
