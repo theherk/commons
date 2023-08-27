283987f348812a3ed2cbc5facbf37b07a5cd38f7
Adam Sherwood <theherk@gmail.com>
2023-08-27 15:09:37 +0200

    Show hidden files by default in neotree.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -91,4 +91,19 @@ return {
       }
     end,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {},
+          never_show = {},
+        },
+      },
+    },
+  },
 }
