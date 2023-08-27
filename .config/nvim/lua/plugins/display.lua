d6cab70b4ee12a4db7a1837c9387ae35eff237e9
Adam Sherwood <theherk@gmail.com>
2023-08-27 15:09:14 +0200

    Set night variant by default.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -12,6 +12,7 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
+      style = "night",
       styles = {
         sidebars = "transparent",
         floats = "transparent",
