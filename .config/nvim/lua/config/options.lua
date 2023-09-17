4cb44844ee9fcd45fb6d7c0c8029ba632fce46c1
Adam Sherwood <theherk@gmail.com>
2023-09-17 23:04:46 +0200

    Neovide cursor effects. 🤨

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -64,3 +64,11 @@ vim.g.neovide_background_color = "#16161e" .. alpha()
 if vim.g.neovide then
   vim.cmd("cd ~/Downloads/")
 end
+-- vim.g.neovide_cursor_vfx_mode = "pixiedust"
+-- vim.g.neovide_cursor_vfx_particle_density = 23.0
+-- vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
+vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_particle_curl = 0.66
+vim.g.neovide_cursor_vfx_particle_density = 23.0
+vim.g.neovide_cursor_vfx_particle_lifetime = 1.33
+vim.g.neovide_cursor_vfx_particle_phase = 11.1
