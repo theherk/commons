460ac2c9f1e0cfe250d94d20b24278659d6aa9a6
Adam Sherwood <theherk@gmail.com>
2023-09-17 22:44:57 +0200

    Show more path in lualine.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -308,7 +308,7 @@ return {
               },
             },
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+            { "filename", path = 4, symbols = { modified = "  ", readonly = "", unnamed = "" } },
             {
               function()
                 return require("nvim-navic").get_location()
