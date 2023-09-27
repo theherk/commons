e5849d1590f6524f3fbb3ab2f0fc29c0c342e7e9
Adam Sherwood <theherk@gmail.com>
2023-09-27 09:56:25 +0200

    Load the lang plugins.

---
+++ .config/nvim/lua/plugins/lang/init.lua
@@ -0,0 +1,11 @@
+require("plugins.lang.docker")
+require("plugins.lang.go")
+require("plugins.lang.java")
+require("plugins.lang.json")
+require("plugins.lang.python")
+require("plugins.lang.rust")
+require("plugins.lang.terraform")
+require("plugins.lang.typescript")
+require("plugins.lang.yaml")
+
+return {}
