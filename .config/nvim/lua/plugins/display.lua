038897a57f1adacb235b15bae9d312e8f902accb
Adam Sherwood <theherk@gmail.com>
2023-10-19 15:50:11 +0200

    Migrate ibl to v3.
    https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -256,22 +256,25 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
+    main = "ibl",
     opts = {
-      char = "│",
-      filetype_exclude = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        }
       },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+      indent = {
+        char = "│",
+      }
     },
   },
   { "MunifTanjim/nui.nvim", lazy = true },
