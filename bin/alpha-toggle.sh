6e6cc80b899d6543322ae9fa83e6c7e34c87b8df
Adam Sherwood <theherk@gmail.com>
2024-01-13 21:35:26 +0100

    Disable nvim transparency toggling.
    It actually works better to have this remain enabled while disabling
    only in the terminal.

--- bin/alpha-toggle.sh
+++ bin/alpha-toggle.sh
@@ -10,9 +10,9 @@ obfuscate() {
 	echo "obfuscating"
 	sed -i '' 's/\(doom\/set-frame-opacity \).*)/\1100)/' .config/doom/config.org
 	sed -i '' 's/\(theme = "base16\)_transparent"/\1_terminal"/' .config/helix/config.toml
-	sed -i '' 's/\(transparent = \)true/\1false/' .config/nvim/lua/plugins/display.lua
-	sed -i '' 's/\(sidebars = \)"transparent"/\1"normal"/' .config/nvim/lua/plugins/display.lua
-	sed -i '' 's/\(floats = \)"transparent"/\1"normal"/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(transparent = \)true/\1false/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(sidebars = \)"transparent"/\1"normal"/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(floats = \)"transparent"/\1"normal"/' .config/nvim/lua/plugins/display.lua
 	sed -i '' 's/\(config.window_background_opacity = \).*/\11.0/' .config/wezterm/theme.lua
 }
 
@@ -20,9 +20,9 @@ clarify() {
 	echo "clarifying"
 	sed -i '' 's/\(doom\/set-frame-opacity \).*)/\194)/' .config/doom/config.org
 	sed -i '' 's/\(theme = "base16\)_terminal"/\1_transparent"/' .config/helix/config.toml
-	sed -i '' 's/\(transparent = \)false/\1true/' .config/nvim/lua/plugins/display.lua
-	sed -i '' 's/\(sidebars = \)"normal"/\1"transparent"/' .config/nvim/lua/plugins/display.lua
-	sed -i '' 's/\(floats = \)"normal"/\1"transparent"/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(transparent = \)false/\1true/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(sidebars = \)"normal"/\1"transparent"/' .config/nvim/lua/plugins/display.lua
+	# sed -i '' 's/\(floats = \)"normal"/\1"transparent"/' .config/nvim/lua/plugins/display.lua
 	sed -i '' 's/\(config.window_background_opacity = \).*/\10.94247/' .config/wezterm/theme.lua
 }
 
