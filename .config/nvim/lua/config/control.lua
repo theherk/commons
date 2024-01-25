a842df56ba5c7070e58fb5a71edae748076a5f0a
Adam Sherwood <theherk@gmail.com>
2024-01-25 17:56:38 +0100

    Add navigation keymaps.

--- .config/nvim/lua/config/control.lua
+++ .config/nvim/lua/config/control.lua
@@ -4,11 +4,21 @@ local Util = require("config.util")
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
--- Move to window using the <ctrl> hjkl keys.
-vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "Go to left window" })
-vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "Go to lower window" })
-vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "Go to upper window" })
-vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "Go to right window" })
+vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope git_files<cr>", { desc = "files (git)"})
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", { desc = "find files"})
+
+-- Navigation of windows, tabs, and buffers.
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "toggle tree"})
+vim.keymap.set("n", "<c-p>", "<cmd>Neotree toggle<cr>", { desc = "toggle tree"})
+vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "left window" })
+vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "lower window" })
+vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "upper window" })
+vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "right window" })
+vim.keymap.set("n", "<tab>c", "<cmd>tabclose<cr>", { desc = "close tab" })
+vim.keymap.set("n", "<tab>f", "<cmd>tabfirst<cr>", { desc = "first tab" })
+vim.keymap.set("n", "<tab>l", "<cmd>tablast<cr>", { desc = "last tab" })
+vim.keymap.set("n", "<tab>n", "<cmd>tabnext<cr>", { desc = "next tab" })
+vim.keymap.set("n", "<tab>p", "<cmd>tabprevious<cr>", { desc = "previous tab" })
 
 -- Resize window using <ctrl> arrow keys.
 vim.keymap.set("n", "<c-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
