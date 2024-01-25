f30e2707cc24cf910a70c36f47b3d3aabe728a53
Adam Sherwood <theherk@gmail.com>
2024-01-25 17:42:44 +0100

    Update init, testing telescope write issue.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,20 @@
 require("config.options")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then vim.fn.system({
-  "git",
-  "clone",
-  "--filter=blob:none",
-  "https://github.com/folke/lazy.nvim.git",
-  "--branch=stable",
-  lazypath,
-}) end
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  defaults = {
-    version = false, -- always use the latest git commit
-  },
+  defaults = { version=false },
+  spec = { { import = "plugins" } },
   install = { colorscheme = { "tokyonight" } },
 })
-
-require("config.autocmds")
 require("config.control")
-
 vim.cmd.colorscheme("tokyonight")
