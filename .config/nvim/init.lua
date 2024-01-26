17bccf7c9bb4e22f7134124d28f3d959e15ed6c6
Adam Sherwood <theherk@gmail.com>
2024-01-26 12:44:18 +0100

    Add autocmds back.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,19 @@
 require("config.options")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
+if not vim.loop.fs_stat(lazypath) then vim.fn.system({
+  "git",
+  "clone",
+  "--filter=blob:none",
+  "https://github.com/folke/lazy.nvim.git",
+  "--branch=stable",
+  lazypath,
+}) end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  defaults = { version=false },
+  defaults = { version = false },
   spec = { { import = "plugins" } },
   install = { colorscheme = { "tokyonight" } },
 })
+require("config.autocmds")
 require("config.control")
 vim.cmd.colorscheme("tokyonight")
