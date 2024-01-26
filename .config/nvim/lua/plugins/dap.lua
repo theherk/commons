2e43da0847afa7ba06a7588f351515f64350f77f
Adam Sherwood <theherk@gmail.com>
2024-01-26 13:47:49 +0100

    Add dd dapui shorcut.

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -8,6 +8,7 @@ return {
       "rcarriga/nvim-dap-ui",
       -- stylua: ignore
       keys = {
+        { "<leader>dd", function() require("dapui").toggle({}) end, desc = "dap ui" },
         { "<leader>du", function() require("dapui").toggle({}) end, desc = "dap ui" },
         { "<leader>de", function() require("dapui").eval() end, desc = "eval", mode = { "n", "v" } },
       },
@@ -16,15 +17,9 @@ return {
         local dap = require("dap")
         local dapui = require("dapui")
         dapui.setup(opts)
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open({})
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-          dapui.close({})
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-          dapui.close({})
-        end
+        dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open({}) end
+        dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close({}) end
+        dap.listeners.before.event_exited["dapui_config"] = function() dapui.close({}) end
       end,
     },
 
@@ -95,10 +90,7 @@ return {
 
     for name, sign in pairs(icons.dap) do
       sign = type(sign) == "table" and sign or { sign }
-      vim.fn.sign_define(
-        "Dap" .. name,
-        { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-      )
+      vim.fn.sign_define("Dap" .. name, { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] })
     end
   end,
 }
