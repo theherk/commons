175ff835631979e1a7c4ecf7b02aeb6daea0c569
Adam Sherwood <theherk@gmail.com>
2024-01-26 12:23:33 +0100

    Add import treesitter init function for early setup.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -191,9 +191,18 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
@@ -248,9 +257,7 @@ return {
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
-  {
-    "Wansmer/treesj",
-  },
+  { "Wansmer/treesj" },
   {
     "williamboman/mason.nvim",
     opts = {
