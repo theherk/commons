94ce2044145a1a445139b249cfcfa5a429e42f60
Adam Sherwood <theherk@gmail.com>
2024-01-26 14:41:33 +0100

    Add trouble toggle.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -68,6 +68,7 @@ return {
     cmd = "TroubleToggle",
     opts = { use_diagnostic_signs = true },
     keys = {
+      { "<leader>ut", "<cmd>TroubleToggle<cr>", desc = "toggle trouble" },
       { "[q", "vim.cmd.cprev", desc = "previous quickfix" },
       { "]q", "vim.cmd.cnext", desc = "next quickfix" },
     },
