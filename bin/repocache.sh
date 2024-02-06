60ae3eb028977cb619e81fa4384442e4a01bbddd
Adam Sherwood <theherk@gmail.com>
2024-02-06 11:13:11 +0100

    Quote project entries in list for repocache.

--- bin/repocache.sh
+++ bin/repocache.sh
@@ -1,7 +1,7 @@
 cat $HOME/commons/.projects-extra | envsubst >$HOME/.projects && fd -HI -d 6 "^\.git\$" $P | xargs dirname 2>/dev/null >>$HOME/.projects
 
 if command -v zoxide >/dev/null; then
-	cat $HOME/.projects | xargs zoxide add
+	cat $HOME/.projects | xargs -I {} zoxide add "{}"
 else
 	echo "zoxide not available; not adding paths"
 fi
