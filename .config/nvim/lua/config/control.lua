9a5e2ec868e62f3f0ad6574b191cc2dd73cd84c7
Adam Sherwood <theherk@gmail.com>
2024-02-15 12:39:45 +0100

    Add better redraw top and bottom.
    See: https://vi.stackexchange.com/a/44135/22390

--- .config/nvim/lua/config/control.lua
+++ .config/nvim/lua/config/control.lua
@@ -8,6 +8,45 @@ vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "quit all" })
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+local function scrolloff_should_reenable()
+  if vim.w.orig_scrolloff == nil then return false end -- Nothing to do.
+  if vim.fn.winheight(0) <= vim.w.orig_scrolloff then return true end -- Too small for override.
+  return vim.w.orig_scrolloff < vim.fn.winline() and vim.fn.winline() < vim.fn.winheight(0) - vim.w.orig_scrolloff
+end
+
+local function scrolloff_add_autocmd()
+  vim.api.nvim_create_augroup("h4s_scrolloff_enhanced", { clear = true })
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = "h4s_scrolloff_enhanced",
+    callback = function()
+      if scrolloff_should_reenable() then
+        vim.opt.scrolloff = vim.w.orig_scrolloff
+        vim.w.orig_scrolloff = nil
+        return true -- Remove the autocmd.
+      end
+    end,
+  })
+end
+
+local function scrolloff_disable()
+  vim.w.orig_scrolloff = vim.opt.scrolloff:get()
+  vim.opt.scrolloff = 0
+  scrolloff_add_autocmd()
+end
+
+local function scroll_very_bottom()
+  scrolloff_disable()
+  vim.cmd(":norm! zb")
+end
+
+local function scroll_very_top()
+  scrolloff_disable()
+  vim.cmd(":norm! zt")
+end
+
+vim.keymap.set("n", "zb", scroll_very_bottom, { desc = "better bottom" })
+vim.keymap.set("n", "zt", scroll_very_top, { desc = "better top" })
+
 -- Navigation of windows, tabs, and buffers.
 vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "left window" })
 vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "lower window" })
