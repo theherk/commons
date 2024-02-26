84eea7deee67b78cb495efcc9a5acb54c5a2efd8
Adam Sherwood <theherk@gmail.com>
2024-02-26 16:34:56 +0100

    Remove unnecessary call to setup.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -148,7 +148,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function(_, opts) require("tokyonight").setup(opts) end,
     opts = {
       transparent = true,
       style = "night",
