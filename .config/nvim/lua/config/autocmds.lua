e11b8b5437dada135e62734e2a9a367e252e43b3
Adam Sherwood <theherk@gmail.com>
2024-02-29 13:25:09 +0100

    Autoformat for lua.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,4 @@
-local function augroup(name)
-  return vim.api.nvim_create_augroup("h4s_" .. name, { clear = true })
-end
+local function augroup(name) return vim.api.nvim_create_augroup("h4s_" .. name, { clear = true }) end
 
 -- Check if we need to reload the file when it changed.
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
@@ -21,9 +19,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 -- Highlight on yank.
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup("highlight_yank"),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
 })
 
 -- Resize splits if window got resized.
@@ -42,14 +38,10 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     local exclude = { "gitcommit" }
     local buf = vim.api.nvim_get_current_buf()
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
-      return
-    end
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
   end,
 })
 
@@ -77,15 +69,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Conceal more for certain filetypes.
+-- Colorize some filetypes on load.
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("conceal_norg"),
   pattern = {
     "norg",
   },
-  callback = function(_)
-    vim.opt.conceallevel = 3
-  end,
+  callback = function(_) vim.opt.conceallevel = 3 end,
 })
 
 -- Autoload populated journal in neorg.
@@ -108,9 +98,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("auto_create_dir"),
   callback = function(event)
-    if event.match:match("^%w%w+://") then
-      return
-    end
+    if event.match:match("^%w%w+://") then return end
     local file = vim.loop.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
