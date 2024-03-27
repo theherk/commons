c319f0d387ad4ed55a1b770b4248b7a92e6e230a
Adam Sherwood <theherk@gmail.com>
2024-03-27 20:20:28 +0100

    Set markdown conceallevel to 2 (max allowed for obsidian.nvim).

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,4 @@ vim.keymap.set("v", "<localleader>l", "<esc><cmd>ObsidianLink<cr>", { desc = "Ob
 vim.keymap.set("v", "<localleader>t", ":!pandoc -t markdown-simple_tables<cr>", { desc = "fmt table" })
 vim.keymap.set("n", "<localleader>T", toc, { desc = "insert toc" })
 vim.keymap.set("v", "<localleader>T", "d:normal <localleader>T<cr>", {})
+vim.opt_local.conceallevel = 2
