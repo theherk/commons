7035f2b3b4064cdcf2f25496e555ebcd09975242
Adam Sherwood <theherk@gmail.com>
2024-04-10 23:57:17 +0200

    Move the command line window down.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -121,9 +121,41 @@ return {
       },
       presets = {
         bottom_search = true,
-        command_palette = true,
         long_message_to_split = true,
         inc_rename = true,
+        lsp_doc_border = true,
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 16,
+            col = "50%",
+          },
+          size = {
+            min_width = 60,
+            width = "auto",
+            height = "auto",
+          },
+        },
+        cmdline_popupmenu = {
+          relative = "editor",
+          position = {
+            row = 19,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+            max_height = 15,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+          },
+        },
       },
     },
     -- stylua: ignore
