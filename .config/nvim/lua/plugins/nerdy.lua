3eb5a556b28ee8bce18c5a9c193bee7e74c42583
Adam Sherwood <theherk@gmail.com>
2024-04-19 16:32:45 +0200

    Use <c-s-i> in place of <c-i> since that sends <tab>.

--- .config/nvim/lua/plugins/nerdy.lua
+++ .config/nvim/lua/plugins/nerdy.lua
@@ -8,7 +8,7 @@ return {
     config = function() require("telescope").load_extension("nerdy") end,
     cmd = "Nerdy",
     keys = {
-      { "<c-i>", "<cmd>Telescope nerdy<cr>", mode = { "i", "n" }, desc = "icons (telescope)" },
+      { "<c-s-i>", "<cmd>Telescope nerdy<cr>", mode = { "i", "n" }, desc = "icons (telescope)" },
       -- { "<leader>si", function() require("telescope").extensions.nerdy.nerdy() end, desc = "icons" },
       { "<leader>si", "<cmd>Telescope nerdy<cr>", desc = "icons (telescope)" },
       { "<leader>sI", "<cmd>Nerdy<cr>", desc = "icons (select)" },
