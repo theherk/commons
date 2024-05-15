757e6501cfdaacf725819b4a0a92537925ef27f7
Adam Sherwood <theherk@gmail.com>
2024-05-15 16:20:51 +0200

    Add zellij launcher to manage session for the current project.

---
+++ bin/zellij-project.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+if set -q ZELLIJ_SESSION_NAME
+    echo "Already in zellij session."
+else
+    set -l p (basename (git root))
+    if contains $p (zellij list-sessions -ns)
+        zellij attach $p
+    else
+        zellij -s $p
+    end
+end
