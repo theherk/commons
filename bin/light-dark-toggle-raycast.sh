8bfb819eba4be9c1820d09e3a73165597c631686
Adam Sherwood <theherk@gmail.com>
2024-05-23 21:22:35 +0200

    Add raycast appearance toggle script command.

---
+++ bin/light-dark-toggle-raycast.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title light-dark-toggle
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author theherk
+# @raycast.authorURL https://raycast.com/theherk
+
+~/bin/light-dark-toggle.sh
