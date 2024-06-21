0e5136a6347b32f6bd03bb79e05f7a76ef0d2ee2
Adam Sherwood <theherk@gmail.com>
2024-06-21 13:38:41 +0200

    Default to partial alpha.

--- .config/wezterm/theme.lua
+++ .config/wezterm/theme.lua
@@ -29,9 +29,9 @@ end
 scheme.cursor_bg = colors.active_fg
 
 scheme.tab_bar = {
-  background = colors.bg, -- tab_bar
+  background = "none", -- tab_bar
   new_tab = {
-    bg_color = colors.bg, -- new_tab
+    bg_color = "none", -- new_tab
     fg_color = colors.hl_2,
   },
   active_tab = {
@@ -66,7 +66,7 @@ function module.apply_to_config(config)
   config.tab_bar_at_bottom = true
   config.tab_max_width = 96
   config.use_fancy_tab_bar = false
-  config.window_background_opacity = 1.0
+  config.window_background_opacity = 0.85247
   config.window_decorations = "RESIZE"
 end
 
