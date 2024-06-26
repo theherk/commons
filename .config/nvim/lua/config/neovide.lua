901cdc75aa9bb30359badc45d1b1923366e26430
Adam Sherwood <theherk@gmail.com>
2024-06-26 14:41:07 +0200

    Add background setters for Neovide.

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,6 +1,12 @@
 vim.g.neovide_transparency = 0.85247
 vim.g.neovide_theme = "auto"
 
+-- Background is none and comes from the terminal.
+-- When running Neovide, it must be set.
+-- These are bespoke to specific colorschemes.
+if vim.g.colors_name == "tokyonight" then vim.api.nvim_set_hl(0, "Normal", { bg = require("tokyonight.colors").setup().bg }) end
+if vim.g.colors_name == "catppuccin-latte" then vim.api.nvim_set_hl(0, "Normal", { bg = require("catppuccin.palettes").get_palette("latte").base }) end
+
 -- Cursor
 
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
