d2ceff7dc2038b4e0d778b963e8cc273b524f247
Adam Sherwood <theherk@gmail.com>
2024-09-23 13:33:19 +0200

    Disable unix socket usage until flicker is sorted.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,12 +18,12 @@ local paths = {
 
 config = {
   check_for_updates = false,
-  default_gui_startup_args = { "connect", "unix" },
+  -- default_gui_startup_args = { "connect", "unix" },
   set_environment_variables = {
     LG_CONFIG_FILE = wezterm.home_dir .. "/.config/lazygit/config.yml",
     PATH = table.concat(paths, ":"),
   },
-  unix_domains = { { name = "unix" } },
+  -- unix_domains = { { name = "unix" } },
 }
 
 control.apply_to_config(config)
