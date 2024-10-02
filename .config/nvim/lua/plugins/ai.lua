3c4ad622bbe01c7c4a57d5642d6582a96a7a604b
Adam Sherwood <theherk@gmail.com>
2024-10-02 16:25:17 +0200

    Lock codeium plugin for now.

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -3,6 +3,7 @@ return {
     "Exafunction/codeium.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = true,
+    commit = "937667b2",
     enabled = vim.fn.getcwd():find("dnb.no") == nil and vim.fn.filereadable(".codeium-enabled") == 1,
   },
   {
