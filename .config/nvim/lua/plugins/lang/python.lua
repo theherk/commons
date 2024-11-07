e040087686531e4db2b259d273cf5f6b19c40c20
Adam Sherwood <theherk@gmail.com>
2024-11-07 15:59:16 +0100

    Remove ruff_lsp.

--- .config/nvim/lua/plugins/lang/python.lua
+++ .config/nvim/lua/plugins/lang/python.lua
@@ -10,18 +10,8 @@ return {
     opts = {
       servers = {
         pyright = {},
-        ruff_lsp = {},
       },
-      setup = {
-        ruff_lsp = function()
-          require("config.util").on_attach(function(client, _)
-            if client.name == "ruff_lsp" then
-              -- Disable hover in favor of Pyright
-              client.server_capabilities.hoverProvider = false
-            end
-          end)
-        end,
-      },
+      setup = {},
     },
   },
   {
