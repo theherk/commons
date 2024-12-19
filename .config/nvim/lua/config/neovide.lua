cb882747f01ffe8b8ddd2857925b48c657ab4bc1
Adam Sherwood <theherk@gmail.com>
2024-12-19 16:29:08 +0100

    Improve color setup for neovide.

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,3 +1,5 @@
+local Util = require("config.util")
+
 vim.g.neovide_detach_on_quit = "always_detach"
 vim.g.neovide_transparency = 0.85247
 vim.g.neovide_theme = "auto"
@@ -5,16 +7,26 @@ vim.g.neovide_theme = "auto"
 -- Background is none and comes from the terminal.
 -- When running Neovide, it must be set.
 -- These are bespoke to specific colorschemes.
-vim.notify(vim.g.colors_name, "info")
-if vim.g.colors_name == "tokyonight-night" then vim.api.nvim_set_hl(0, "Normal", { bg = require("tokyonight.colors").setup().bg }) end
-if vim.g.colors_name == "catppuccin-latte" then vim.api.nvim_set_hl(0, "Normal", { bg = require("catppuccin.palettes").get_palette("latte").base }) end
+-- vim.notify(vim.g.colors_name, "info")
+if vim.g.colors_name == "catppuccin-frappe" then
+  local colors = require("catppuccin.palettes").get_palette("frappe")
+  vim.api.nvim_set_hl(0, "Normal", { bg = colors.base })
+  Util.set_term_colors(colors)
+end
+if vim.g.colors_name == "catppuccin-latte" then
+  local colors = require("catppuccin.palettes").get_palette("latte")
+  vim.api.nvim_set_hl(0, "Normal", { bg = colors.base })
+  Util.set_term_colors(colors)
+end
+if vim.g.colors_name == "tokyonight-night" then
+  local colors = require("tokyonight.colors").setup()
+  vim.api.nvim_set_hl(0, "Normal", { bg = colors.bg })
+  Util.set_term_colors(colors)
+end
 
--- Cursor
-
--- vim.g.neovide_cursor_vfx_mode = "pixiedust"
--- vim.g.neovide_cursor_vfx_particle_density = 23.0
--- vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
-
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_smooth_blink = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.g.neovide_cursor_vfx_particle_curl = 0.66
 vim.g.neovide_cursor_vfx_particle_density = 23.0
