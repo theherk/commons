b16368232c8adeffe45370374908742e6dd7ccc9
Adam Sherwood <theherk@gmail.com>
2024-12-19 16:27:36 +0100

    Fix telescope to use fzf.
    I don't know how this was missed before. Very strange.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -248,6 +248,10 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
+    end,
     opts = {
       defaults = {
         layout_config = {
