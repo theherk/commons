7d83549ebc4a41c149bf9b1ee9eb53e016664671
Adam Sherwood <theherk@gmail.com>
2024-12-19 16:26:28 +0100

    Improve note naming and frontmatter for obsidian.nvim.

--- .config/nvim/lua/plugins/note.lua
+++ .config/nvim/lua/plugins/note.lua
@@ -127,19 +127,28 @@ return {
         alias_format = "%Y-%m-%d",
         folder = "journal",
       },
+      disable_frontmatter = true,
       follow_url_func = function(url) vim.fn.jobstart({ "open", url }) end,
+      note_frontmatter_func = function(note)
+        local out = { tags = note.tags }
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
       note_id_func = function(title)
-        -- Given "Note Name" will make ID `<timestamp>-note-name`.
-        -- Filename is the same with `.md` suffix.
-        local suffix = ""
         if title ~= nil then
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          return title
         else
+          local suffix = ""
           for _ = 1, 4 do
             suffix = suffix .. string.char(math.random(65, 90))
           end
+          return tostring(os.time()) .. "-" .. suffix
         end
-        return tostring(os.time()) .. "-" .. suffix
       end,
       open_app_foreground = true,
       preferred_link_style = "markdown",
