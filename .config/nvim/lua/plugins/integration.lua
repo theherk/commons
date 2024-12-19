670b4cd81601aa0d67e9d581b90d2db4f4122da2
Adam Sherwood <theherk@gmail.com>
2024-12-19 16:31:05 +0100

    Add new toggleterm extension and configuration.

--- .config/nvim/lua/plugins/integration.lua
+++ .config/nvim/lua/plugins/integration.lua
@@ -1,3 +1,15 @@
 return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      shade_terminals = false,
+    },
+    keys = {
+      { "<c-`>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "terminal (horizontal)", mode = { "i", "x", "n", "s", "t" } },
+      { "<d-j>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "terminal (horizontal)", mode = { "i", "x", "n", "s", "t" } },
+      { "<ds-j>", "<cmd>ToggleTerm direction=tab<cr>", desc = "terminal (tab)", mode = { "i", "x", "n", "s", "t" } },
+    },
+  },
   { "glacambre/firenvim", build = ":call firenvim#install(0)" },
 }
