b86a766017f67f4d93b6aaa79c4e6b50fc7ac687
Adam Sherwood <theherk@gmail.com>
2024-12-20 19:43:24 +0100

    Add project directory and simplify lualine configuration.
    Since I won't have the current project listed outside wezterm or zellij,
    this can help me keep on top of what project I'm in.

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -358,16 +358,11 @@ return {
           theme = custom,
           globalstatus = true,
           disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+          component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
         sections = {
-          lualine_a = {
-            {
-              "mode",
-              separator = { left = "" },
-              right_padding = 2,
-            },
-          },
+          lualine_a = { { "mode" } },
           lualine_b = { "branch" },
           lualine_c = {
             {
@@ -379,54 +374,52 @@ return {
                 hint = icons.diagnostics.Hint,
               },
             },
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 4, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+            { "filetype", icon_only = true },
+            { "filename", path = 4, separator = { right = "▷" }, symbols = { modified = "  ", readonly = "", unnamed = "" } },
             {
               function() return require("nvim-navic").get_location() end,
               cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-              padding = { left = 1, right = 0 },
             },
           },
           lualine_x = {
-            -- stylua: ignore
             {
               function() return require("noice").api.status.command.get() end,
               cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
               color = Util.fg("Statement"),
             },
-            -- stylua: ignore
             {
               function() return require("noice").api.status.mode.get() end,
               cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
               color = Util.fg("Constant"),
             },
-            -- stylua: ignore
             {
               function() return "  " .. require("dap").status() end,
               cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
               color = Util.fg("Debug"),
             },
             { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.fg("Special") },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-            },
+            { "diff", symbols = { added = icons.git.added, modified = icons.git.modified, removed = icons.git.removed } },
           },
           lualine_y = {
-            { "progress", separator = " ", padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-          lualine_z = {
+            { "progress" },
+            { "location" },
             {
-              function() return " " .. os.date("%R") end,
-              separator = { right = "" },
-              left_padding = 2,
+              function()
+                if not vim.g.neovide then return "" end
+                local handle = io.popen("git rev-parse --show-toplevel 2> /dev/null")
+                if handle then
+                  local result = handle:read("*a")
+                  handle:close()
+                  if result and result ~= "" then
+                    local repo_name = result:match("[^/]+$"):gsub("\n", "")
+                    return "󱂬 " .. repo_name
+                  end
+                end
+                return ""
+              end,
             },
           },
+          lualine_z = { { function() return " " .. os.date("%R") end } },
         },
       }
     end,
