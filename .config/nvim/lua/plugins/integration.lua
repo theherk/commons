98187e8ea5d9ecfcae49023629248ed813c028f9
Adam Sherwood <theherk@gmail.com>
2025-01-06 12:27:48 +0100

    Increase terminal size for ToggleTerm in horizontal.

--- .config/nvim/lua/plugins/integration.lua
+++ .config/nvim/lua/plugins/integration.lua
@@ -4,6 +4,13 @@ return {
     version = "*",
     opts = {
       shade_terminals = false,
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 22
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
     },
     keys = {
       { "<c-`>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "terminal (horizontal)", mode = { "i", "x", "n", "s", "t" } },
