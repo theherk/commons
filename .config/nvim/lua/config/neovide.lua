ee562dfd9090db3a32e6ef4ff6bc7ed6e764ada9
Adam Sherwood <theherk@gmail.com>
2025-01-13 10:22:26 +0100

    Add keybinding for external project switching using telescope.

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -35,3 +35,33 @@ vim.g.neovide_cursor_vfx_particle_curl = 0.66
 vim.g.neovide_cursor_vfx_particle_density = 23.0
 vim.g.neovide_cursor_vfx_particle_lifetime = 1.33
 vim.g.neovide_cursor_vfx_particle_phase = 11.1
+
+vim.keymap.set("n", "<c-p>", function()
+  local cmd = [[zoxide query -l | rg --color=never -FxNf ~/.projects | sed "s:$HOME:~:" ]]
+  local dirs = vim.fn.systemlist(cmd)
+
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  pickers
+    .new({}, {
+      prompt_title = "Projects",
+      finder = finders.new_table({
+        results = dirs,
+      }),
+      sorter = conf.generic_sorter({}),
+      attach_mappings = function(prompt_bufnr)
+        actions.select_default:replace(function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          local dir = vim.fn.expand(selection[1])
+          vim.fn.system({ "open", "-a", "NeovideProject", dir })
+        end)
+        return true
+      end,
+    })
+    :find()
+end)
