59c1765712704c870e0c06da1bfd6aec37197018
Adam Sherwood <theherk@gmail.com>
2025-01-20 18:16:56 +0100

    Remove old neovim configuration.

--- .config/nvim-hold/after/ftplugin/markdown.lua
+++
@@ -1,9 +0,0 @@
-local function toc() vim.cmd([[ exe 'r!md-toc --indent 2 % | rg -N --color=never . | head -c -1' ]]) end
-
-vim.keymap.set("v", "<localleader>l", "<esc><cmd>ObsidianLink<cr>", { desc = "ObsidianLink" })
-vim.keymap.set("v", "<localleader>t", ":!pandoc -f markdown-simple_tables+pipe_tables -t markdown-grid_tables-multiline_tables-simple_tables+pipe_tables<cr>", { desc = "format table" })
-vim.keymap.set("n", "<localleader>T", toc, { desc = "insert toc" })
-vim.keymap.set("v", "<localleader>T", "d:normal <localleader>T<cr>", { desc = "insert toc" })
-
-vim.g.markdown_folding = 1
-vim.opt_local.conceallevel = 2
--- .config/nvim-hold/lua/config/display.lua
+++
@@ -1,2 +0,0 @@
-vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE", ctermbg = "NONE" })
