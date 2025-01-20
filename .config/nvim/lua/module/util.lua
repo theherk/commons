50a90a1572a329b5adc33413e0a2fc9b4973e422
Adam Sherwood <theherk@gmail.com>
2025-01-20 10:42:23 +0100

    Add back accidentally removed get_clients routine.

--- .config/nvim/lua/module/util.lua
+++ .config/nvim/lua/module/util.lua
@@ -112,6 +112,18 @@ function M.fg(name)
   return fg and { fg = string.format("#%06x", fg) }
 end
 
+function M.get_clients(opts)
+  local ret = {}
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ---@diagnostic disable-next-line: deprecated
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then ret = vim.tbl_filter(function(client) return client.supports_method(opts.method, { bufnr = opts.bufnr }) end, ret) end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
 -- returns the root directory based on:
 -- * lsp workspace folders
 -- * lsp root_dir
