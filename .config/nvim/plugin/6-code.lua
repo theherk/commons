d5b3443b90281c39c54c7a8a6e83ea8ee2e149d5
Adam Sherwood <theherk@gmail.com>
2025-01-21 17:49:09 +0100

    Improve lua lsp configuration.

--- .config/nvim/plugin/6-code.lua
+++ .config/nvim/plugin/6-code.lua
@@ -332,8 +332,22 @@ later(function()
     lua_ls = {
       settings = {
         Lua = {
-          workspace = { checkThirdParty = false },
-          completion = { callSnippet = "Replace" },
+          runtime = {
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+          telemetry = {
+            enable = false,
+          },
         },
       },
     },
