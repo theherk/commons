49bcb5485d4ab2fedcdc8defb1abafcfac907478
Adam Sherwood <theherk@gmail.com>
2025-01-23 13:34:02 +0100

    Improve neogit configuration.

--- .config/nvim/plugin/6-code.lua
+++ .config/nvim/plugin/6-code.lua
@@ -242,9 +242,10 @@ later(function()
       ["dnb.ghe.com"] = "https://dnb.ghe.com/${owner}/${repository}/compare/${branch_name}?expand=1",
       ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
       ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-      ["gitlab.tech.dnb.no"] = "https://gitlab.tech.dnb.no/${path}/${repository}/-/merge_requests/new?merge_request[source_branch]=${branch_name}&merge_request[target_branch]=main",
+      ["gitlab.tech.dnb.no"] = "https://gitlab.tech.dnb.no/${path}/${repository}/-/merge_requests/new?merge_request[source_branch]=${branch_name}",
     },
     graph_style = "unicode",
+    use_per_project_settings = true,
   })
   vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "neogit" })
 end)
