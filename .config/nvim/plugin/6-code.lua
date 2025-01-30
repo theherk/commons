819f82ce6443341622d27defb743c2ac73a9abb2
Adam Sherwood <theherk@gmail.com>
2025-01-30 15:09:25 +0100

    Run deepseek in ollama locally.

--- .config/nvim/plugin/6-code.lua
+++ .config/nvim/plugin/6-code.lua
@@ -155,6 +155,15 @@ later(function()
   require("avante").setup({
     file_selector = { provider = "mini.pick" },
     windows = { sidebar_header = { enabled = false } },
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        __inherited_from = "openai",
+        api_key_name = "",
+        endpoint = "http://127.0.0.1:11434/v1",
+        model = "deepseek-r1:8b",
+      },
+    },
   })
 end)
 
