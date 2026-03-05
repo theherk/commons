9c5ce1ffe3a0009a6f7938fbda48a71b66e60fd8
Adam Sherwood <theherk@gmail.com>
2026-03-05 16:27:05 +0100

    Remove hide more highlighting

--- .config/nvim/plugin/6-code.lua
+++ .config/nvim/plugin/6-code.lua
@@ -163,18 +163,7 @@ later(function()
           "vimdoc",
           "yaml",
         },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-          disable = function(lang, buf)
-            if vim.g.neovide then
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then return true end
-            end
-            return false
-          end,
-        },
+        highlight = { enable = true },
         incremental_selection = {
           enable = true,
           keymaps = {
