d09f1d989377ded698755f66753a90130455270d
Adam Sherwood <theherk@gmail.com>
2026-03-18 08:29:48 +0100

    fix(daily-note): Remove delays in note switch

--- bin/daily-note.sh
+++ bin/daily-note.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! pgrep -q "[Ww]ezterm"; then
-    open -a WezTerm
-    sleep 1.5
-fi
-
 osascript -e '
 tell application "WezTerm" to activate
-delay 0.3
 tell application "System Events"
   keystroke "d" using command down
 end tell
