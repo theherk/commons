4357a25a31358ce9f6ddf97f1a8e461a4aa51490
Adam Sherwood <herk@theherk.com>
2026-06-22 10:16:10 +0200

    Detect system appearance in xbar load plugin

--- bin/xbar/load.10s.fish
+++ bin/xbar/load.10s.fish
@@ -3,14 +3,26 @@
 
 set load (sysctl -n vm.loadavg | string match -ra '[\d.]+')
 
-# Catppuccin Frappe: green=#a6d189 peach=#ef9f76 red=#e78284
+# Detect appearance and pick Catppuccin palette
+# Frappe (dark): green=#a6d189 peach=#ef9f76 red=#e78284
+# Latte (light): green=#40a02b peach=#fe640b red=#d20f39
+if defaults read -g AppleInterfaceStyle &>/dev/null
+    set green "#a6d189"
+    set peach "#ef9f76"
+    set red "#e78284"
+else
+    set green "#40a02b"
+    set peach "#fe640b"
+    set red "#d20f39"
+end
+
 set l1_int (math --scale=0 $load[1])
 if test $l1_int -ge 18
-    set color "#e78284"
+    set color $red
 else if test $l1_int -ge 9
-    set color "#ef9f76"
+    set color $peach
 else
-    set color "#a6d189"
+    set color $green
 end
 
 echo "  $load[1] $load[2] $load[3] | font=VictorMonoNF-Regular color=$color"
